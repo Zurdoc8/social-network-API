5981cd183396e89e835391d5e0096a9ea1d5fbf9
Andres <garelan@gmail.com>
2022-06-05 16:43:19 -0500

    created boiler plate server.js file

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const db = require('./models');
+
+app.use('/api', require('./routes'))
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('debug', true);
+
+app.listen(PORT, () => {
+    console.log('Now running on port ${PORT}!');
+});
